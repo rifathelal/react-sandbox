d4d9f021b78484fd5adebf17f3b918c33538d12b
Rifat Helal <rifathelal@outlook.com>
2023-02-28 03:19:00 +0600

    [Draggable ship WIP] Added draggable button

--- src/components/Battleships.tsx
+++ src/components/Battleships.tsx
@@ -1,22 +1,63 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export default function Battleships() {
   const [playerBoard, setPlayerBoard] = useState(Array(100).fill("") as string[])
   const [cpuBoard, setCpuBoard] = useState(Array(100).fill("") as string[])
+  
+  function dragElement(elem: HTMLElement) {
+    var initialX = "0px", initialY = "0px", mx = 0, my = 0;
+    elem.onmousedown = setDraggable;
 
-  const ships = [
-    { size: 5, isHorizontal: false, x: 0, y: 0 },
-    { size: 4, isHorizontal: false, x: 0, y: 0 },
-    { size: 3, isHorizontal: false, x: 0, y: 0 },
-    { size: 3, isHorizontal: false, x: 0, y: 0 },
-    { size: 2, isHorizontal: false, x: 0, y: 0 },
-  ]
-  const [playerShips, setPlayerShips] = useState(ships);
-  const onShipClick = (index: number) => {
-    const newShips = ships.slice();
-    newShips[index].isHorizontal = !newShips[index].isHorizontal;
-    setPlayerShips(newShips);
-  } 
+    function setDraggable(e: MouseEvent) {
+      e = e || window.event;
+      e.preventDefault();
+      mx = e.clientX
+      my = e.clientY
+      initialX = elem.style.top;
+      initialY = elem.style.left;
+      document.onmousemove = drag;
+      elem.onmouseup = setUndraggable;
+    }
+
+    function drag(e: MouseEvent) {
+      e = e || window.event;
+      e.preventDefault();
+      let newX = mx - e.clientX;
+      let newY = my - e.clientY;
+      mx = e.clientX;
+      my = e.clientY
+      elem.style.top = (elem.offsetTop - newY) + "px";
+      elem.style.left = (elem.offsetLeft - newX) + "px";
+    }
+
+    function setUndraggable() {
+      elem.style.top = initialY;
+      elem.style.left = initialX;
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  }
+
+  const draggable = useRef(null as HTMLButtonElement|null);
+  useEffect(() => {
+    if (!draggable.current) return;
+    console.log("bruh")
+    dragElement(draggable.current)
+  }, [draggable.current])
+
+  // const ships = [
+  //   { size: 5, isHorizontal: false, x: 0, y: 0 },
+  //   { size: 4, isHorizontal: false, x: 0, y: 0 },
+  //   { size: 3, isHorizontal: false, x: 0, y: 0 },
+  //   { size: 3, isHorizontal: false, x: 0, y: 0 },
+  //   { size: 2, isHorizontal: false, x: 0, y: 0 },
+  // ]
+  // const [playerShips, setPlayerShips] = useState(ships);
+  // const onShipClick = (index: number) => {
+  //   const newShips = ships.slice();
+  //   newShips[index].isHorizontal = !newShips[index].isHorizontal;
+  //   setPlayerShips(newShips);
+  // } 
   
   return (
     <div className="flex items-center justify-center h-full gap-16">
@@ -26,13 +67,14 @@ export default function Battleships() {
         }
       </ul>
       <ul className="flex flex-col gap-2">
-        { ships.map((ship, index) => 
+        {/* { ships.map((ship, index) => 
           <button 
             onClick={() => { onShipClick(index) }} 
             className="h-8 border border-secondary"
             style={{width: `${ship.size * 2}rem`}}
           ></button>
-        )}
+        )} */}
+        <button className="absolute" ref={draggable}>Bruh</button>
       </ul>
     </div>
   )
